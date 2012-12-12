79975fcb4c6ebbb62ef0dd613839f3138d337d49
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2012-12-12 14:41:21 +0100

    Used the common xacml profile class

--- src/main/java/org/glite/ce/cream/authz/argus/ESActionMapping.java
+++ src/main/java/org/glite/ce/cream/authz/argus/ESActionMapping.java
@@ -22,8 +22,9 @@ import java.util.Iterator;
 
 import javax.xml.namespace.QName;
 
-import org.glite.authz.common.model.Attribute;
+import org.glite.authz.common.profile.CommonXACMLAuthorizationProfileConstants;
 import org.glite.authz.pep.profile.AbstractAuthorizationProfile;
+import org.glite.authz.pep.profile.CommonXACMLAuthorizationProfile;
 import org.glite.ce.commonj.authz.AuthZConstants;
 import org.glite.ce.commonj.authz.AuthorizationException;
 import org.glite.ce.commonj.authz.argus.ActionMappingInterface;
@@ -35,6 +36,16 @@ public class ESActionMapping
 
     private final static String DELEGATION_NS = "http://www.gridsite.org/namespaces/delegation-21";
 
+    public final static String ACTION_ES_CREATION = "http://www.eu-emi.eu/es/2010/12/creation";
+
+    public final static String ACTION_ES_ACTIVITY = "http://www.eu-emi.eu/es/2010/12/activity";
+
+    public final static String ACTION_ES_ACT_MAN = "http://www.eu-emi.eu/es/2010/12/activitymanagement";
+
+    public final static String ACTION_ES_RES_INFO = "http://www.eu-emi.eu/es/2010/12/resourceinfo";
+
+    public final static String ACTION_ES_DELEGATE = "http://www.gridsite.org/namespaces/delegation-21";
+
     public static final String[] mandProperties = { AuthZConstants.LOCAL_USER_ID, AuthZConstants.LOCAL_GROUP_ID };
 
     public String getXACMLAction(QName operation) {
@@ -44,33 +55,33 @@ public class ESActionMapping
         if (opNS.startsWith(ES_NS)) {
 
             if (opName.equals("CreateActivity")) {
-                return ESAuthorizationProfile.ACTION_ES_CREATION;
+                return ACTION_ES_CREATION;
             }
 
             if (opName.equals("ListActivities") || opName.equals("GetActivityStatus")
                     || opName.equals("GetActivityInfo")) {
-                return ESAuthorizationProfile.ACTION_ES_ACTIVITY;
+                return ACTION_ES_ACTIVITY;
             }
 
             if (opName.equals("CancelActivity") || opName.equals("WipeActivity")) {
-                return ESAuthorizationProfile.ACTION_ES_ACT_MAN;
+                return ACTION_ES_ACT_MAN;
             }
 
             if (opName.equals("PauseActivity") || opName.equals("ResumeActivity")) {
-                return ESAuthorizationProfile.ACTION_ES_ACT_MAN;
+                return ACTION_ES_ACT_MAN;
             }
 
             if (opName.equals("RestartActivity") || opName.equals("NotifyService")) {
-                return ESAuthorizationProfile.ACTION_ES_ACT_MAN;
+                return ACTION_ES_ACT_MAN;
             }
 
             if (opName.equals("GetResourceInfo") || opName.equals("QueryResourceInfo")) {
-                return ESAuthorizationProfile.ACTION_ES_ACT_MAN;
+                return ACTION_ES_ACT_MAN;
             }
 
         } else if (opNS.equals(DELEGATION_NS)) {
 
-            return ESAuthorizationProfile.ACTION_ES_DELEGATE;
+            return ACTION_ES_DELEGATE;
         }
 
         return null;
@@ -78,13 +89,13 @@ public class ESActionMapping
 
     public String getAttributeMapping(String obId, String attrId) {
 
-        if (ESAuthorizationProfile.ID_POSIX_ENV_MAP.equals(obId)) {
+        if (CommonXACMLAuthorizationProfileConstants.ID_OBLIGATION_MAP_POSIX_USER.equals(obId)) {
 
-            if (ESAuthorizationProfile.ID_USERID_ASSIGN.equals(attrId)) {
+            if (CommonXACMLAuthorizationProfileConstants.ID_ATTRIBUTE_USER_ID.equals(attrId)) {
                 return AuthZConstants.LOCAL_USER_ID;
             }
 
-            if (ESAuthorizationProfile.ID_PGROUPID_ASSIGN.equals(attrId)) {
+            if (CommonXACMLAuthorizationProfileConstants.ID_ATTRIBUTE_PRIMARY_GROUP_ID.equals(attrId)) {
                 return AuthZConstants.LOCAL_GROUP_ID;
             }
         }
@@ -122,61 +133,7 @@ public class ESActionMapping
     }
 
     public AbstractAuthorizationProfile getProfile() {
-        return new ESAuthorizationProfile();
+        return CommonXACMLAuthorizationProfile.getInstance();
     }
 
-    public class ESAuthorizationProfile
-        extends AbstractAuthorizationProfile {
-
-        public final static String ACTION_ES_CREATION = "http://www.eu-emi.eu/es/2010/12/creation";
-
-        public final static String ACTION_ES_ACTIVITY = "http://www.eu-emi.eu/es/2010/12/activity";
-
-        public final static String ACTION_ES_ACT_MAN = "http://www.eu-emi.eu/es/2010/12/activitymanagement";
-
-        public final static String ACTION_ES_RES_INFO = "http://www.eu-emi.eu/es/2010/12/resourceinfo";
-
-        public final static String ACTION_ES_DELEGATE = "http://www.gridsite.org/namespaces/delegation-21";
-
-        public final static String ID_POSIX_ENV_MAP = "http://dci-sec.org/xacml/obligation/map-local-user/posix";
-
-        public final static String ID_PROFILE = "http://dci-sec.org/xacml/attribute/profile-id";
-
-        public final static String ID_USERID_ASSIGN = "http://dci-sec.org/xacml/attribute/user-id";
-
-        public final static String ID_GROUPID_ASSIGN = "http://dci-sec.org/xacml/attribute/group-id";
-
-        public final static String ID_PGROUPID_ASSIGN = "http://dci-sec.org/xacml/attribute/group-id/primary";
-
-        public final static String ES_XACML_PROFILE = "http://dci-sec.org/xacml/profile/common-authz/1.1";
-
-        public ESAuthorizationProfile() {
-            super(ES_XACML_PROFILE);
-        }
-
-        public String getProfileIdAttributeIdentifer() {
-            return ID_PROFILE;
-        }
-
-        protected String getSubjectKeyInfoDatatype() {
-            return Attribute.DT_STRING;
-        }
-
-        public String getMapUserToPOSIXEnvironmentObligationIdentifier() {
-            return ID_POSIX_ENV_MAP;
-        }
-
-        public String getUserIdAttributeAssignmentIdentifier() {
-            return ID_USERID_ASSIGN;
-        }
-
-        public String getGroupIdAttributeAssignmentIdentifier() {
-            return ID_GROUPID_ASSIGN;
-        }
-
-        public String getPrimaryGroupIdAttributeAssignmentIdentifier() {
-            return ID_PGROUPID_ASSIGN;
-        }
-
-    }
 }
